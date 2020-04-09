38c4e44a939c713dd632c2904ed8cb694ba2be79
hacialidemirbas <hacialidemirbas@gmail.com>
2020-04-09 13:37:24 -0700

    review

---
+++ src/April9Revision/MainPerson.java
@@ -0,0 +1,14 @@
+package April9Revision;
+
+public class MainPerson {
+    public static void main(String[] args) {
+        Person obj=new Person();
+        Person obj1= new Person();
+//obj.setName("Ali");
+obj1.setName1("Veli");
+
+        System.out.println(obj.name);
+        System.out.println(obj1.getName1());
+
+    }
+}
---
+++ src/April9Revision/Person.java
@@ -0,0 +1,24 @@
+package April9Revision;
+
+public class Person {
+    String name;
+    int age;
+    int ssn;
+    private String name1;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
+    }
+}
